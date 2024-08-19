c4577d2f44ad823c672dca83a85998c5d05c5379
Ian Kinsey <ian@aikbix.com>
2024-08-18 21:01:07 -0400

    Update hostname

--- web/src/util.tsx
+++ web/src/util.tsx
@@ -1,7 +1,7 @@
 import { CalendarEvent, Event } from "./models";
 
 export async function request(url: string, body?: any) {
-    url = "http://localhost:9898" + url
+    url = "http://" + window.location.hostname + ":9898" + url
     const headers = {
         'Content-Type': 'application/json',
     };
